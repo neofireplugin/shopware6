caa5b8a6b2d65b384cb898e497101e23b2dd3199
Patrick Weihe <info@neofire.de>
2020-01-12 19:24:50 +0100

    Add files via upload

---
+++ src/Storefront/Controller/ClearCartController.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Swag\StorefrontController\Storefront\Controller;
+
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Storefront\Controller\StorefrontController;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @RouteScope(scopes={"storefront"})
+ */
+
+class ClearCartController extends StorefrontController
+{
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    /**
+     * @Route("/plentymarkets", name="plentymarkets", options={"seo"="false"}, methods={"GET"})
+     */
+    public function import()
+    {
+
+      //  $output =  $this->systemConfigService->get('StorefrontController.config.plentymarketsuser');
+
+        $output = 'Shopware 6';
+
+
+        return $this->renderStorefront('@Storefront/storefront/page/checkout/cart/index.html.twig', ['output' => $output]);
+    }
+}
